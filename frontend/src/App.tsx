7036cfa8f7c8ba76e0afa17e691b4c641c03d74c
Jonathan da Silva <jonathansilva15sp@gmail.com>
2022-09-10 17:56:34 -0300

    Criando Componente Card de Vendas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
       <Header/>
-      {/* <main>
+      <main>
         <section id="sales">
           <div className="dsmeta-container">
-
+            <SalesCard/>
           </div>
         </section>
-      </main> */}
+      </main>
     </>
   )
 }
