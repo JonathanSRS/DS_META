e5598f8280368e2bcbe2a6bbb444d978ce56e15f
Jonathan da Silva <jonathansilva15sp@gmail.com>
2022-09-10 20:44:35 -0300

    Customizando Busca por Data

--- backend/src/main/java/com/project/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/project/dsmeta/repositories/SalesRepository.java
@@ -1,9 +1,17 @@
 package com.project.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.project.dsmeta.entities.Sale;
 
 public interface SalesRepository extends JpaRepository<Sale, Long>{
-
+	//Método customizado
+	@Query("SELECT obj FROM Sale obj WHERE obj.data BETWEEN :min AND :max ORDER BY obj.total DESC")
+	//Páginando Resultados 
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pegeable);
 }
